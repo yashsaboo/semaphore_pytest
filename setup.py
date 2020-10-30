c1d2a303871882873a405216da951389e8a53442
Yash Saboo <yashsaboo99@gmail.com>
2020-10-29 19:09:03 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 setup(
- name = ‘cs527_Prep’,
- packages = [‘cs527_Prep’],
- version = ‘1.0.0’
+ name = 'cs527_Prep',
+ packages = ['cs527_Prep'],
+ version = '1.0.0'
 )
