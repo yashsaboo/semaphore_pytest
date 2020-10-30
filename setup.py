ba55d21c6e90a70657e99855ccb6abce8ab38a75
Yash Saboo <yashsaboo99@gmail.com>
2020-10-29 19:18:58 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
- name = 'cs527_Prep',
- packages = ['cs527_Prep'],
+ name = 'semaphore_pytest',
+ packages = ['semaphore_pytest'],
  version = '1.0.0',
  url = 'https://github.com/yashsaboo/semaphore_pytest/archive/1.0.0.tar.gz'
 )
